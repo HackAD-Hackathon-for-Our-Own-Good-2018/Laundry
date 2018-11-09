fea954eb381eedd51503496513fdf58b0e855ab1
Prajjwal Bhattarai <taoprajjwal@gmail.com>
2018-11-10 03:52:03 +0400

    Changed to postreg

--- OnStart.py
+++ OnStart.py
@@ -1,18 +1,34 @@
 import sys
-import sqlite3
+import psycopg2
+import urllib.parse as urlparse
 import datetime
 
-conn=sqlite3.connect("tasks.db")
+url = urlparse.urlparse("postgres://vteynwmfbgkmrk:4c42cad3159fafc6dafb1e14f1c8654e60ea6097284e8e2cd60418cd0fdf26c7@ec2-54-83-27-162.compute-1.amazonaws.com:5432/dce9rsp8t7n6cm")
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
+
+conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+            )
+
 cursor=conn.cursor()
+
 
 machine_ID=int(sys.argv[1])
 duration=int(sys.argv[2])
 uid=sys.argv[3]
 
-print(machine_ID+"MACHINE ID")
-print(duration+"This is the duration")
+print(str(machine_ID)+"MACHINE ID")
+print(str(duration)+"This is the duration")
 
-sql_string="INSERT INTO `Machines` VALUES (?,?,?,?)"
+sql_string="INSERT INTO machines VALUES (%s,%s,%s,%s)"
 now_dt=datetime.datetime.utcnow()
 cursor.execute(sql_string,(machine_ID,str(now_dt),now_dt+datetime.timedelta(minutes=duration),uid))
 conn.commit()
--- timer.py
+++ timer.py
@@ -1,12 +1,28 @@
-import sqlite3
+import psycopg2
+import urllib.parse as urlparse
 import datetime
 import smtplib
 import email.utils
 from email.mime.text import MIMEText
 import sys
+import os
 
+#url=urlparse.urlparse(os.environ['DATABASE_URL'])
+url = urlparse.urlparse("postgres://vteynwmfbgkmrk:4c42cad3159fafc6dafb1e14f1c8654e60ea6097284e8e2cd60418cd0fdf26c7@ec2-54-83-27-162.compute-1.amazonaws.com:5432/dce9rsp8t7n6cm")
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
 
-conn=sqlite3.connect("tasks.db")
+conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+            )
+
 cursor=conn.cursor()
 
 def send_email(email_recieve):
@@ -29,20 +45,21 @@ def send_email(email_recieve):
 
 def delete_row(Machine_ID):
     print(str(Machine_ID)+"Is getting deleted")
-    cursor.execute("DELETE FROM `Machines` WHERE `MachineID`=?", str(Machine_ID))
+    cursor.execute("DELETE FROM machines WHERE machine_id=%s", str(Machine_ID))
     conn.commit()
     conn.close()
 
 def scheduler_check():
-    sql_string="SELECT * FROM `Machines`"
+    sql_string="SELECT machine_id, date_added, date_ended, user_id FROM public.machines"
     cursor.execute(sql_string)
+    print(cursor)
     rows=cursor.fetchall()
+    print(rows)
     for row in rows:
         print(row)
         if datetime.datetime.strptime(row[2],"%Y-%m-%d %H:%M:%S.%f")<datetime.datetime.utcnow():
             print("Getting deleted")
             send_email(row[3])
             delete_row(row[0])
-
 
 scheduler_check()
