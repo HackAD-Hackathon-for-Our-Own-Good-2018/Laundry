e1327fbed57e3dd945597eea32ec6a6f01160136
ao1385 <ao1385@nyu.edu>
2018-11-09 23:20:44 +0400

    automated

--- index.js
+++ index.js
@@ -16,23 +16,28 @@ express()
   .get('/db.ejs', (req,res) => res.render('pages/db'))
   .get('/registration.ejs', (req, res) => res.render('pages/registration'))
   .post('/save', (req, res) =>{
-    console.log(req.body);
+    sendEmail(req.body);
     res.send(JSON.stringify({result:'OK'}));
   }
 )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-// var promise1 = new Promise(function(resolve, reject){
-//   var email = 'ao1385@nyu.edu';
-//   var msg = 'msg';
-//   var sendData = [email, msg];
-//   resolve(sendData);
-// });
-//
-// promise1.then(function(sendData){
-//   const pythonProcess = spawn('python',["send_email.py", sendData[0], sendData[1]]);
-//   pythonProcess.stdout.on('data', (data) => {
-//       // Do something with the data returned from python script
-//       console.log(data.toString());
-//   });
-// });
+function sendEmail(data){
+  // console.log(data);
+
+  var promise1 = new Promise(function(resolve, reject){
+    var email = data.netID + '@nyu.edu';
+    var msg = data.building;
+    var sendData = [email, msg];
+    resolve(sendData);
+  });
+
+  promise1.then(function(sendData){
+    const pythonProcess = spawn('python',["send_email.py", sendData[0], sendData[1]]);
+    pythonProcess.stdout.on('data', (data) => {
+        // Do something with the data returned from python script
+        console.log(data.toString());
+    });
+  });
+
+}
