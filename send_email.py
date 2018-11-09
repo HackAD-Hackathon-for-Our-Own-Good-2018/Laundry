030bb6ac7d0cb261e9c76c94b626b776f7ec0661
Prajjwal Bhattarai <taoprajjwal@gmail.com>
2018-11-09 21:05:27 +0400

    Changed sender email address

--- send_email.py
+++ send_email.py
@@ -1,16 +1,21 @@
 import smtplib
 import sys
+import email.utils
+from email.mime.text import MIMEText
 
 email_recieve=sys.argv[1]
-msg=sys.argv[2]
 
-email_send="taoprajjwal@gmail.com"
-password="climacteric2@"
+msg=MIMEText("This is a reminder that your laundry has been completed. Please go pick it up.")
+msg['From']=email.utils.formataddr(("Laundry","laundrynyuad@gmail.com"))
+msg['Subject']="Laundry Notification"
+
+email_send="laundrynyuad@gmail.com"
+password="<~{z)s@H6k.aMh!b"
 
 server=smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
 server.login(email_send,password)
 
-server.sendmail(email_send,email_recieve,msg)
+server.sendmail(email_send,email_recieve,msg.as_string())
 print("Done")
 server.quit()
