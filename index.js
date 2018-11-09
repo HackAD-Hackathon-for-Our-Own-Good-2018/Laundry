185edb589b187b1d23584cafc8710727ca3e1d66
ao1385 <ao1385@nyu.edu>
2018-11-09 23:09:25 +0400

    connect

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const spawn = require("child_process").spawn;
+const bodyParser = require("body-parser");
 //for python script
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
@@ -13,6 +15,11 @@ express()
   .get('/av.ejs', (req, res) => res.render('pages/av'))
   .get('/db.ejs', (req,res) => res.render('pages/db'))
   .get('/registration.ejs', (req, res) => res.render('pages/registration'))
+  .post('/save', (req, res) =>{
+    console.log(req.body);
+    res.send(JSON.stringify({result:'OK'}));
+  }
+)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // var promise1 = new Promise(function(resolve, reject){
