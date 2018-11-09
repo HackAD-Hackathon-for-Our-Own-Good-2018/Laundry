92ad962fc3d6030644f0f039eccdb8263e6f90f2
Prajjwal Bhattarai <taoprajjwal@gmail.com>
2018-11-10 00:41:03 +0400

    Fixed more issues with OnStart..I promise this is the last one

--- OnStart.py
+++ OnStart.py
@@ -6,8 +6,8 @@ conn=sqlite3.connect("tasks.db")
 cursor=conn.cursor()
 
 
-machine_ID=sys.argv[1]
-duration=sys.argv[2]
+machine_ID=int(sys.argv[1])
+duration=int(sys.argv[2])
 uid=sys.argv[3]
 
 sql_string="INSERT INTO `Machines` VALUES (?,?,?,?)"
