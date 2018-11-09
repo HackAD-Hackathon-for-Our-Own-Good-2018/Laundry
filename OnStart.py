a341e4dc21cefab001ed2be79ed29b942f46b721
Prajjwal Bhattarai <taoprajjwal@gmail.com>
2018-11-10 01:49:25 +0400

    Debugging onStart

--- OnStart.py
+++ OnStart.py
@@ -9,6 +9,9 @@ machine_ID=int(sys.argv[1])
 duration=int(sys.argv[2])
 uid=sys.argv[3]
 
+print(machine_ID+"MACHINE ID")
+print(duration+"This is the duration")
+
 sql_string="INSERT INTO `Machines` VALUES (?,?,?,?)"
 now_dt=datetime.datetime.utcnow()
 cursor.execute(sql_string,(machine_ID,str(now_dt),now_dt+datetime.timedelta(minutes=duration),uid))
