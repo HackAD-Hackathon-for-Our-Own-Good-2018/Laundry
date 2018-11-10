3851f9a7743e2a61ec36e49bd7709cedf6124c85
Prajjwal Bhattarai <taoprajjwal@gmail.com>
2018-11-10 04:44:42 +0400

    Fix minor changes

--- OnStart.py
+++ OnStart.py
@@ -3,7 +3,8 @@ import psycopg2
 import urllib.parse as urlparse
 import datetime
 
-url = urlparse.urlparse("postgres://vteynwmfbgkmrk:4c42cad3159fafc6dafb1e14f1c8654e60ea6097284e8e2cd60418cd0fdf26c7@ec2-54-83-27-162.compute-1.amazonaws.com:5432/dce9rsp8t7n6cm")
+url=urlparse.urlparse(os.environ['DATABASE_URL'])
+#url = urlparse.urlparse("postgres://vteynwmfbgkmrk:4c42cad3159fafc6dafb1e14f1c8654e60ea6097284e8e2cd60418cd0fdf26c7@ec2-54-83-27-162.compute-1.amazonaws.com:5432/dce9rsp8t7n6cm")
 dbname = url.path[1:]
 user = url.username
 password = url.password
