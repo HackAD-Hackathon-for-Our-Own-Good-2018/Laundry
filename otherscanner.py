45f1f5b812fd86b3a29da7204ff6a19e32cea827
jumana <jai264@nyu.edu>
2018-11-10 09:07:02 +0400

    Scannerapp

---
+++ otherscanner.py
@@ -0,0 +1,14 @@
+import zbar
+import numpy
+from PIL import Image
+
+scanner = zbar.ImageScanner()
+pil = Image.open('test3.jpg').convert('L')
+width, height = pil.size
+raw = pil.tobytes()
+image = zbar.Image(width, height, 'Y800', raw)
+result = scanner.scan(image)
+
+for symbol in image:
+    print symbol.data.decode(u'utf-8')
+
