7305360c16d25a75218af4f9e2b28ac8421c1d77
Prajjwal Bhattarai <taoprajjwal@gmail.com>
2018-11-10 10:13:53 +0400

    Fix

--- OnStart.py
+++ OnStart.py
@@ -1,10 +1,10 @@
 import sys
 import psycopg2
-import urllib.parse as urlparse
-# try:
-#     from urllib.parse import urlparse
-# except ImportError:
-     # from urlparse import urlparse
+#import urllib.parse as urlparse
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 import datetime
 import os
 
@@ -15,16 +15,18 @@ machine_ID=int(sys.argv[1])
 duration=int(sys.argv[2])
 uid=sys.argv[3]
 
+print(machine_ID, duration, uid)
 
-url= urlparse(os.environ['DATABASE_URL'])
-#url = urlparse("postgres://vteynwmfbgkmrk:4c42cad3159fafc6dafb1e14f1c8654e60ea6097284e8e2cd60418cd0fdf26c7@ec2-54-83-27-162.compute-1.amazonaws.com:5432/dce9rsp8t7n6cm")
+
+#url= urlparse(os.environ['DATABASE_URL'])
+url = urlparse("postgres://vteynwmfbgkmrk:4c42cad3159fafc6dafb1e14f1c8654e60ea6097284e8e2cd60418cd0fdf26c7@ec2-54-83-27-162.compute-1.amazonaws.com:5432/dce9rsp8t7n6cm")
+print(url)
 dbname = url.path[1:]
 user = url.username
 password = url.password
 host = url.hostname
 port = url.port
 
-print(machine_ID, duration, uid)
 
 conn = psycopg2.connect(
             dbname=dbname,
