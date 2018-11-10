71b2c095f6dee8d671d6fe5e627200e6e458b2dc
Prajjwal Bhattarai <taoprajjwal@gmail.com>
2018-11-10 09:49:21 +0400

    Fix

--- OnStart.py
+++ OnStart.py
@@ -11,7 +11,7 @@ import os
 print("We are now in python")
 print(sys.argv)
 
-url= ""(os.environ['DATABASE_URL'])
+url= os.environ['DATABASE_URL']
 #url = urlparse.urlparse("postgres://vteynwmfbgkmrk:4c42cad3159fafc6dafb1e14f1c8654e60ea6097284e8e2cd60418cd0fdf26c7@ec2-54-83-27-162.compute-1.amazonaws.com:5432/dce9rsp8t7n6cm")
 dbname = url.path[1:]
 user = url.username
