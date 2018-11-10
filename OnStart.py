ec7f1368702ce5c81a795175d5c66879ad5e13e2
Prajjwal Bhattarai <taoprajjwal@gmail.com>
2018-11-10 10:07:21 +0400

    Fix

--- OnStart.py
+++ OnStart.py
@@ -11,6 +11,11 @@ import os
 print("We are now in python")
 print(sys.argv)
 
+machine_ID=int(sys.argv[1])
+duration=int(sys.argv[2])
+uid=sys.argv[3]
+
+
 url= urlparse(os.environ['DATABASE_URL'])
 #url = urlparse("postgres://vteynwmfbgkmrk:4c42cad3159fafc6dafb1e14f1c8654e60ea6097284e8e2cd60418cd0fdf26c7@ec2-54-83-27-162.compute-1.amazonaws.com:5432/dce9rsp8t7n6cm")
 dbname = url.path[1:]
@@ -19,7 +24,7 @@ password = url.password
 host = url.hostname
 port = url.port
 
-print(sys.argv)
+print(machine_ID, duration, uid)
 
 conn = psycopg2.connect(
             dbname=dbname,
@@ -33,10 +38,6 @@ cursor=conn.cursor()
 
 print(cursor)
 
-machine_ID=int(sys.argv[1])
-duration=int(sys.argv[2])
-uid=sys.argv[3]
-print(machine_ID, duration, uid)
 
 print(str(machine_ID)+"MACHINE ID")
 print(str(duration)+"This is the duration")
